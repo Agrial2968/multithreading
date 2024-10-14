0c407187b6cc51100c3dd2a88670ae1c04d0c769
Agrial <alexandrovger@yandex.ru>
2024-10-14 15:18:36 +0300

    task 9

---
+++ Navigation/BruteForce.swift
@@ -0,0 +1,8 @@
+//
+//  BruteForce.swift
+//  Navigation
+//
+//  Created by user on 10.10.2024.
+//
+
+import Foundation
---
+++ Navigation/Library/ExtensionString.swift
@@ -0,0 +1,8 @@
+//
+//  ExtensionString.swift
+//  Navigation
+//
+//  Created by user on 10.10.2024.
+//
+
+import Foundation
